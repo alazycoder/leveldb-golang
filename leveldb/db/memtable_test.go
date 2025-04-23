bb443bf9140279033e80132410d2f2beb049e0f3
gaoqian <gaoqian@protonbase.io>
2025-04-24 01:37:58 +0800

    amend the last commit

--- leveldb/db/memtable_test.go
+++ leveldb/db/memtable_test.go
@@ -10,25 +10,25 @@ func TestSimpleReadWrite(t *testing.T) {
 	memTable := NewMemTable()
 
 	var valueType ValueType
-	// var value Slice
+	var value Slice
 
-	//valueType, _ = memTable.Get(NewLookupKey([]byte("foo"), 1))
-	//assert.Equal(t, valueTypeNotExist, valueType)
-	//
-	//memTable.Add(1, valueTypeValue, []byte("foo"), []byte("v1"))
-	//valueType, value = memTable.Get(NewLookupKey([]byte("foo"), 1))
-	//assert.Equal(t, valueTypeValue, valueType)
-	//assert.Equal(t, []byte("v1"), []byte(value))
-	//
-	//memTable.Add(2, valueTypeValue, []byte("foo"), []byte(""))
-	//valueType, value = memTable.Get(NewLookupKey([]byte("foo"), 2))
-	//assert.Equal(t, valueTypeValue, valueType)
-	//assert.Equal(t, []byte(""), []byte(value))
-	//
-	//memTable.Add(3, valueTypeValue, []byte("bar"), []byte("v2"))
-	//valueType, value = memTable.Get(NewLookupKey([]byte("bar"), 3))
-	//assert.Equal(t, valueTypeValue, valueType)
-	//assert.Equal(t, []byte("v2"), []byte(value))
+	valueType, _ = memTable.Get(NewLookupKey([]byte("foo"), 1))
+	assert.Equal(t, valueTypeNotExist, valueType)
+
+	memTable.Add(1, valueTypeValue, []byte("foo"), []byte("v1"))
+	valueType, value = memTable.Get(NewLookupKey([]byte("foo"), 1))
+	assert.Equal(t, valueTypeValue, valueType)
+	assert.Equal(t, []byte("v1"), []byte(value))
+
+	memTable.Add(2, valueTypeValue, []byte("foo"), []byte(""))
+	valueType, value = memTable.Get(NewLookupKey([]byte("foo"), 2))
+	assert.Equal(t, valueTypeValue, valueType)
+	assert.Equal(t, []byte(""), []byte(value))
+
+	memTable.Add(3, valueTypeValue, []byte("bar"), []byte("v2"))
+	valueType, value = memTable.Get(NewLookupKey([]byte("bar"), 3))
+	assert.Equal(t, valueTypeValue, valueType)
+	assert.Equal(t, []byte("v2"), []byte(value))
 
 	memTable.Add(4, valueTypeDeletion, []byte("foo"), []byte(""))
 	valueType, _ = memTable.Get(NewLookupKey([]byte("foo"), 4))
